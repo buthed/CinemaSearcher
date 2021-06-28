012404ba678d9c7ad9db37202f04add4b3b91389
buthed <temaTihonov@yandex.ru>
2021-06-28 20:45:25 +0300

    Before fix Picasso

--- app/src/main/java/com/tematikhonov/cinemasearcher/model/repository/Repository.kt
+++ app/src/main/java/com/tematikhonov/cinemasearcher/model/repository/Repository.kt
@@ -3,6 +3,6 @@ package com.tematikhonov.cinemasearcher.model.repository
 import com.tematikhonov.cinemasearcher.model.entites.Cinema
 
 interface Repository {
-    fun getCinemaFromServer(): Cinema
+    fun getCinemaFromServer(movie_id: Int): Cinema
     fun getCinemaFromLocalStorage(): List<Cinema>
 }
--- app/src/main/java/com/tematikhonov/cinemasearcher/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/tematikhonov/cinemasearcher/model/repository/RepositoryImpl.kt
@@ -2,8 +2,23 @@ package com.tematikhonov.cinemasearcher.model.repository
 
 import com.tematikhonov.cinemasearcher.model.entites.Cinema
 import com.tematikhonov.cinemasearcher.model.entites.getCinemasList
+import com.tematikhonov.cinemasearcher.model.rest_entities.CinemaLoader
 
 class RepositoryImpl : Repository {
-    override fun getCinemaFromServer() = Cinema()
+    override fun getCinemaFromServer(movie_id: Int) : Cinema {
+        val dto = CinemaLoader.loadCinema(movie_id)
+        return Cinema(
+                movie_id = movie_id,
+                title = dto?.title,
+                release_date = dto?.release_date,
+                vote_average = dto?.vote_average,
+                budget = dto?.budget,
+                revenue = dto?.revenue,
+                poster_path = dto?.poster_path,
+                backdrop_path = dto?.backdrop_path,
+                overview = dto?.overview
+        )
+    }
+
     override fun getCinemaFromLocalStorage() = getCinemasList()
 }
