ed02d018c9d7b08b74099b562a23275e28aed243
buthed <temaTihonov@yandex.ru>
2021-08-18 21:54:37 +0300

    So close finish service+broadcast in one file

--- app/src/main/java/com/tematikhonov/cinemasearcher/view/details/CinemaFragment.kt
+++ app/src/main/java/com/tematikhonov/cinemasearcher/view/details/CinemaFragment.kt
@@ -1,36 +1,58 @@
 package com.tematikhonov.cinemasearcher.view
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.squareup.picasso.Picasso
 import com.tematikhonov.cinemasearcher.databinding.CinemaFragmentBinding
 import com.tematikhonov.cinemasearcher.model.CinemaDTO
-import com.tematikhonov.cinemasearcher.view.details.CinemaLoader
-import com.tematikhonov.cinemasearcher.view.details.CinemaLoaderListener
-import com.tematikhonov.cinemasearcher.viewmodel.CinemaViewModel
-import kotlinx.android.synthetic.main.main_fragment.*
+import com.tematikhonov.cinemasearcher.view.details.*
 
-class CinemaFragment : Fragment(), CinemaLoaderListener {
 
-    lateinit var viewModel: CinemaViewModel
+const val THREADS_FRAGMENT_BROADCAST_EXTRA = "FRAGMENT BROADCAST"
+const val DETAILS_ID_EXTRA = "ID"
+const val DETAILS_TITLE_EXTRA = "TITLE"
+const val DETAILS_RELEASE_DATE_EXTRA = "RELEASE_DATE"
+const val DETAILS_VOTE_AVERAGE_EXTRA = "VOTE AVERAGE"
+const val DETAILS_BUDGET_EXTRA = "BUDGET"
+const val DETAILS_REVENUE_EXTRA = "REVENUE"
+const val DETAILS_POSTER_EXTRA = "POSTER"
+const val DETAILS_BACKDROP_EXTRA = "BACKDROP"
+const val DETAILS_OVERVIEW_EXTRA = "OVERVIEW"
+
+
+
+
+class CinemaFragment : Fragment() {
+
+    companion object {
+        const val BUNDLE_EXTRA = "cinema"
+        fun newInstance(bundle: Bundle): CinemaFragment {
+            val fragment = CinemaFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    //lateinit var viewModel: CinemaViewModel
     var _binding:CinemaFragmentBinding? = null
     private val binding:CinemaFragmentBinding
         get() :CinemaFragmentBinding{
             return _binding!!
         }
 
-    companion object {
-        const val BUNDLE_EXTRA = "cinema"
 
-        fun newInstance(bundle: Bundle): CinemaFragment {
-            val fragment = CinemaFragment()
-            fragment.arguments = bundle
-            return fragment
-        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context?.let{ LocalBroadcastManager.getInstance(it).registerReceiver(loadResultsReceiver, IntentFilter(DETAILS_INTENT_FILTER))}
     }
 
     override fun onCreateView(
@@ -47,32 +69,64 @@ class CinemaFragment : Fragment(), CinemaLoaderListener {
         _binding = null
     }
 
-    override fun onLoaded(cinemaDTO: CinemaDTO) {
-        with(binding) {
-            cardTitle.text = cinemaDTO.title
-            cardYear.text = cinemaDTO.release_date
-            cardRank.text = cinemaDTO.vote_average
-            cardOverview.text = cinemaDTO.overview
-            cardBudget.text = "${cinemaDTO.budget.toString()} $"
-            cardRevenue.text =  "${cinemaDTO.revenue.toString()} $"
-            val urlPoster: String = "${"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"}" + cinemaDTO.poster_path
-            val urlBackdrop: String = "${"https://www.themoviedb.org/t/p/original/"}" +  cinemaDTO.backdrop_path
-            Picasso.get().load(urlPoster).into(cardPoster)
-            Picasso.get().load(urlBackdrop).into(cardBackdrop)
+
+    private val loadResultsReceiver: BroadcastReceiver = object :BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            intent?.let{
+                when(it.getStringExtra(DETAILS_LOAD_RESULT_EXTRA)) {
+                    DETAILS_RESPONSE_SUCCESS_EXTRA->
+                        renderData(CinemaDTO(
+                                it.getIntExtra(DETAILS_ID_EXTRA, 1),
+                                it.getStringExtra(DETAILS_TITLE_EXTRA)!!,
+                                it.getStringExtra(DETAILS_RELEASE_DATE_EXTRA)!!,
+                                it.getStringExtra(DETAILS_VOTE_AVERAGE_EXTRA)!!,
+                                it.getIntExtra(DETAILS_BUDGET_EXTRA, 1),
+                                it.getIntExtra(DETAILS_REVENUE_EXTRA, 1),
+                                it.getStringExtra(DETAILS_POSTER_EXTRA)!!,
+                                it.getStringExtra(DETAILS_BACKDROP_EXTRA)!!,
+                                it.getStringExtra(DETAILS_OVERVIEW_EXTRA)!!))
+                        else -> null
+                }
+            }
         }
     }
 
-    override fun onFailed(throwable: Throwable) {
-        Toast.makeText(context, throwable.localizedMessage, Toast.LENGTH_LONG).show()
+    fun renderData(cinemaDTO: CinemaDTO){
+        binding.mainView.visibility = View.VISIBLE
+        binding.loadingLayoutForCard.visibility = View.GONE
+
+        cinemaBundle?.let{ cinemaBundle:CinemaDTO->
+            binding.cardTitle.text = cinemaDTO.title.toString()
+            binding.cardYear.text = cinemaDTO.release_date.toString()
+            binding.cardRank.text = cinemaDTO.vote_average.toString()
+            binding.cardOverview.text = cinemaDTO.overview.toString()
+            binding.cardBudget.text = "${cinemaBundle.budget.toString()} $"
+            binding.cardRevenue.text =  "${cinemaBundle.revenue.toString()} $"
+            val urlPoster: String = cinemaBundle.poster_path.toString()
+            val urlBackdrop: String = cinemaBundle.backdrop_path.toString()
+            Picasso.get().load(urlPoster).into(binding.cardPoster)
+            Picasso.get().load(urlBackdrop).into(binding.cardBackdrop);
+        }
+    }
+
+    var cinemaBundle: CinemaDTO? = null
+
+    private fun getCinema(){
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayoutForCard.visibility = View.VISIBLE
+        cinemaBundle?.let{
+            context?.startService(Intent(context, CinemaService::class.java)).apply {  }
+        }
     }
 
     lateinit var cinemaLocal: CinemaDTO
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         arguments?.getParcelable<CinemaDTO>(BUNDLE_EXTRA)?.apply {
-            cinemaLocal = this
-            CinemaLoader(this@CinemaFragment, this.id).loadCinema()
+            cinemaBundle = this
+            getCinema()
         }
     }
 }
