10cca7d906ea77f618d7977d4eeaf86ae69be90c
buthed <temaTihonov@yandex.ru>
2021-10-25 22:00:53 +0300

    With mannual notification

--- app/src/main/java/com/tematikhonov/cinemasearcher/view/MainActivity.kt
+++ app/src/main/java/com/tematikhonov/cinemasearcher/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.tematikhonov.cinemasearcher.view
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,6 +16,10 @@ import com.tematikhonov.cinemasearcher.databinding.ActivityMainBinding
 import com.tematikhonov.cinemasearcher.view.main.MainFragment
 
 class MainActivity: AppCompatActivity() {
+
+    val CHANNEL_ID1="fisrt_channel"
+    val CHANNEL_ID2="second_channel"
+    val NOTIFICATION_ID=1
 
     lateinit var binding: ActivityMainBinding
 
@@ -24,6 +32,7 @@ class MainActivity: AppCompatActivity() {
                     .beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commit()
+
         }
 
         //Initialize the bottom navigation view
@@ -38,5 +47,34 @@ class MainActivity: AppCompatActivity() {
                 R.id.settings_login))
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+        push()
+    }
+
+    private fun push() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID2).apply {
+            setContentTitle("First notification")
+            setContentText("notification")
+            setSmallIcon(R.drawable.ic_kotlin_logo)
+            setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel(notificationManager)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun createChannel(notificationManager: NotificationManager) {
+        val name = "Важное новый2"
+        val description1 = "От этого зависит судьба Земли"
+        val description2 = "Не выключай, Христа ради"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(CHANNEL_ID2, name, importance).apply {
+            description = description1
+        }
+        notificationManager.createNotificationChannel(channel)
     }
 }
